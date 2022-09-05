d06da60c6d7ba43c733df3ff0f44f13c8b2f63b7
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2022-09-05 20:37:37 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
+import Header from "./componets/Header"
 import NotificationButton from "./componets/NotificationButton"
+import SalesCard from "./componets/SalesCard"
 
 
 function App() {
   return (
     <>
-      <h1>Olá, Mundo!!!</h1>
-      <NotificationButton />
-    </>
-  )
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+        </main>
+      </>
+      )
 }
 
-export default App
+      export default App
